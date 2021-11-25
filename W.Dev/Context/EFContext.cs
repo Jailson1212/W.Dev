83d5a6472e4afc718f93c2bf70e4800858b5b5cf
Junior <Junior@DESKTOP-L1K81JK>
2021-11-25 07:10:40 -0300

    Adicionar arquivos de projeto.

---
+++ W.Dev/Context/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using W.Dev.Models;
+
+namespace W.Dev.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Sessão> Sessões { get; set; }
+    }
+}
---
+++ W.Dev/Models/Evento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace W.Dev.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public string Instagram { get; set; }
+        public string Horarios { get; set; }
+        public string Logo { get; set; }
+        public string Banner { get; set; }
+        public TimeSpan Dias { get; set; }
+
+    }
+}
